9c7a3f3a2c91a245531b096d84f5b4c8f17dfa2a
sittiku <sittiku@TITNP10-SITTIKU.true.th>
2014-02-20 14:52:05 +0700

    FULL STEP

--- grails-app/controllers/devtool/SqlAndModelController.groovy
+++ grails-app/controllers/devtool/SqlAndModelController.groovy
@@ -10,10 +10,11 @@ class SqlAndModelController {
     
     def doProcess(){
         String dataIn = params['sqlText'];
-        String[] dataArr = dataIn.split("\n");
+       /* String[] dataArr = dataIn.split("\n");
         StringBuilder sb = new StringBuilder();
-        boolean upch = false;
+       
         for(String tmpStr:dataArr){
+			boolean upch = false;
             tmpStr = tmpStr.toLowerCase();
             for(char c:tmpStr.toCharArray()){
                 if(c == '_'){
@@ -26,11 +27,127 @@ class SqlAndModelController {
                 }
             }
             
-        }
+        }*/
                 
         //dataIn = dataIn.toLdataIn.replaceAll("(_[a-z])","\$1");ower();
-
-        
-        render sb.toString();
+		StringBuilder sb = new StringBuilder();
+		doThimInnerSpace( doTrim( dataIn)).eachLine {
+			sb.append(DBNameToJavaName(it)+"\n")
+		}
+        render doFieldToJavaType(sb.toString())
     }
+	
+	def doTrimAction(){
+		String dataIn = params['sqlText'];
+		render doTrim(dataIn)
+	}
+	def doThimInnerSpaceAction(){
+		String dataIn = params['sqlText'];
+		render doThimInnerSpace(dataIn)
+	}
+	def doFieldToJavaTypeAction(){
+		String dataIn = params['sqlText'];
+		StringBuilder sb = new StringBuilder();
+		dataIn.eachLine {
+			sb.append(DBNameToJavaName(it)+"\n")
+		}
+		render doFieldToJavaType(sb.toString())
+		//render sb.toString()
+	}
+	def doDBGetterAction(){
+		String dataIn = params['sqlText'];
+		render doDBGetter(dataIn)
+	}
+	def doDBSetterAction(){
+		String dataIn = params['sqlText'];
+		render doDBSetter(dataIn)
+	}
+	
+	//------------------------------------FUNCTION------------------------------------
+    
+    def doTrim(String dataIn){
+        String dataout="";
+        dataIn.eachLine{
+            dataout += it.trim()+"\n"
+        }
+        dataout
+    }
+	
+	def doThimInnerSpace(String dataIn){
+		String dataout=dataIn.replaceAll("[ \t]+", " ");
+		dataout
+	}
+	
+
+    
+    def doFieldToJavaType(String dataIn){
+        String dataout="";
+        dataIn.eachLine{
+            dataout += it.replaceAll("^(?i)(varchar2|varchar)","String")
+            .replaceAll("^(?i)date","Date").replaceAll("^(?i)number","long")+"\n"
+        }
+        
+        dataout
+    }
+    
+    def doDBGetter(String dataIn){
+        String dataout="";
+        dataIn.eachLine{
+            String[] tmp = it.split(" ");
+            String type = tmp[0]; //EX String
+            String name = tmp[1]; //EX empId
+            String dbName = javaNameToDBName(name) //EMP_ID
+            String bigName = name.substring(0, 1).toUpperCase() + name.substring(1); //EX EmpId
+            dataout += "tmpInstance.set"+bigName+"(rs.get"+type+"(\""+dbName+"\"));\n"
+        }
+        dataout
+    }
+	
+	def doDBSetter(String dataIn){
+		String dataout="";
+		dataIn.eachLine{
+			String[] tmp = it.split(" ");
+			String type = tmp[0]; //EX String
+			String name = tmp[1]; //EX empId
+			String dbName = javaNameToDBName(name) //EMP_ID
+			String bigName = name.substring(0, 1).toUpperCase() + name.substring(1); //EX EmpId
+			dataout += "cs.set"+type+"(parameterIndex++, "+name+");\n"
+			//cs.setString(parameterIndex++, hrpId);
+		}
+		dataout
+	}
+    
+    def javaNameToDBName(String dataIn){
+        StringBuilder sb = new StringBuilder();
+        char[] charList = dataIn.toCharArray()
+        for(int i = charList.length-1 ; i>=0 ;i--){
+            if(charList[i]>='A' && charList[i]<='Z'){
+                sb.append(charList[i]).append("_");
+            }else if(charList[i]>='a' && charList[i]<='z'){
+                sb.append(String.valueOf((char)((int)charList[i]-32)))
+            }else{
+                sb.append(charList[i]);
+            }
+        }
+        sb.reverse();
+    }
+	
+	def DBNameToJavaName(String dataIn){
+		StringBuilder sb = new StringBuilder();
+		boolean upch = false;
+		dataIn = dataIn.toLowerCase();
+		
+		for(char c:dataIn.toCharArray()){
+			if(c == '_'){
+				upch = true;
+			}else if(upch == true){
+				sb.append(String.valueOf((char)((int)c-32)));
+				upch = false;
+			}else{
+				sb.append(String.valueOf(c));
+			}
+		}
+		
+		sb.toString()
+	}
 }
--- grails-app/views/sqlAndModel/sqltomodel.gsp
+++ grails-app/views/sqlAndModel/sqltomodel.gsp
@@ -15,23 +15,47 @@
     <body style="color: white;background-image: url('../images/sqltomodalbg.jpg');background-size: 100%;">
         
         <div id="wrapper" style="margin: 0 auto;width: 1000px;">
-        <div id="content" style="">
+        <div id="content" style="margin: 0 auto;">
                             <h1>Sql to model</h1>
                             <!--<g:form name="myForm" >
                                 <g:textArea name="sqlText" value="" />
                                 >?
                                 <div id="resultdiv"/>
                                 <g:remoteLink controller="sqlAndModel" action="doProcess" update="resultdiv">myLink</g:remoteLink>
-                            </g:form>-->
+                            </g:form>--><%--
 
 
-                            <g:formRemote name="myForm" url="[controller:'sqlAndModel',action:'doProcess']" update="resultdiv">
+                          <!--  <g:formRemote name="myForm" url="[controller:'sqlAndModel',action:'doProcess']" update="resultdiv"> -->
+                          
+                                --%>
+                                <div style="background-color: white;color: black;">
+	                                <h2>
+		                                SELECT data_type,column_name<br/>
+										FROM all_tab_columns<br/>
+										WHERE TABLE_NAME='USER_TABLE'
+	                                </h2>
+                                </div>
+                                <g:form name="myForm"  style="margin: 0 auto">
                                 <g:textArea name="sqlText" style="width:90%;height:300px;margin: 10px auto;display: block;" value="" /><br/>
-                                <g:submitButton name="submit" style="width:100%;margin: 10px auto;" value="Do Convert"/>
-                            </g:formRemote>
+
+								<button style="visibility: hidden;"></button><br/>
+								
+								<button onclick="$('#sqlText').val($('#resultdiv').val());return false;">^^^ Copy DATA UP ^^^</button><br/>
+                           		<g:submitToRemote url="[controller:'sqlAndModel',action:'doProcess']" update="resultdiv" value="Do Convert (3 STEP))"></g:submitToRemote> |
+                           		<g:submitToRemote url="[controller:'sqlAndModel',action:'doTrimAction']" update="resultdiv" value="Trim"></g:submitToRemote>
+                            	<g:submitToRemote url="[controller:'sqlAndModel',action:'doThimInnerSpaceAction']" update="resultdiv" value="TrimIner"></g:submitToRemote>
+                            	<g:submitToRemote url="[controller:'sqlAndModel',action:'doFieldToJavaTypeAction']" update="resultdiv" value="DB -&gt; JAVA"></g:submitToRemote><BR/> 
+                            	<g:submitToRemote url="[controller:'sqlAndModel',action:'doDBGetterAction']" update="resultdiv" value="GETTER"></g:submitToRemote>
+                            	<g:submitToRemote url="[controller:'sqlAndModel',action:'doDBSetterAction']" update="resultdiv" value="SETTER"></g:submitToRemote>
+                            	
+                            	
                                 <textarea id="resultdiv" style="background-color: #FFF3F3;width:90%;height:300px;margin: 10px auto;display: block;">
                                 </textarea>
-         </div><!-- END content -->
+                                </g:form>
+                               
+                            <%--</g:formRemote>
+                            
+         --%></div><!-- END content -->
          </div><!-- END warpper -->
     </body>
 </html>
